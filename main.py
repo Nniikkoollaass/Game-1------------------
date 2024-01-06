34cedd444615b362dff4618e2ba746b02d715e3a
Nniikkoollaass <nikk1.endurable7@gmail.com>
2024-01-06 16:40:54 +0200

    Initial commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ WIDTH = 1200
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 NEW_COLOR = (0, 255, 0)
+NEW_COLOR_TWO = (125, 0, 125)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGT)) # задаємо розміри екрану
 
